8cf706742cbc99e9b3158f92e32d3d5565e788f2
tempilou <tempels.fabrice@gmail.com>
2021-05-29 08:28:25 +0200

    JS modification police sur element.html

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// RECHERCHE POLICE 1 ET POLICE 2
+const police1=window.getComputedStyle(document.getElementById('police-font-1')).fontFamily;
+const police2=window.getComputedStyle(document.getElementById('police-font-2')).fontFamily;
+
+// MODIFICATION DU TEXTE 
+document.getElementById('police-font-1').innerText=police1;
+document.getElementById('police-font-2').innerText=police2;
